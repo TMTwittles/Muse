65ff852257c64929eb1fd022a04010fdeee7b7f5
TristanM <95954032+TMTwittles@users.noreply.github.com>
2024-12-15 21:55:25 +1100

    MM11: New rest animation for the sword.

--- Source/Muse/Private/MuseCharacter.cpp
+++ Source/Muse/Private/MuseCharacter.cpp
@@ -221,7 +221,7 @@ void AMuseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMuseCharacter::Look);
 
     // LockOn
-    EnhancedInputComponent->BindAction(LockOnAction, ETriggerEvent::Started, this, &AMuseCharacter::EnterLockOn);
+    EnhancedInputComponent->BindAction(LockOnAction, ETriggerEvent::Triggered, this, &AMuseCharacter::EnterLockOn);
 
     // Fire weapon
     //EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &AMuseCharacter::FireWeapon);
