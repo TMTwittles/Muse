68e1eab786b5e4144c44916d1b32c05b72df956f
TristanM <95954032+TMTwittles@users.noreply.github.com>
2025-03-10 17:54:51 +1100

    MM17: Reworked anim blueprints to better handle ranged and melee equipment.

--- Source/Muse/Public/Equipment/RangedAnimationDataAsset.h
+++ Source/Muse/Public/Equipment/RangedAnimationDataAsset.h
@@ -1,21 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "Equipment/EquipmentAnimationDataAsset.h"
-#include "Animation/AimOffsetBlendSpace.h"
+#include "Animation/BlendSpace.h"
 #include "RangedAnimationDataAsset.generated.h"
 
 /**
  * 
  */
-UCLASS()
+UCLASS(BlueprintType)
 class MUSE_API URangedAnimationDataAsset : public UEquipmentAnimationDataAsset
 {
 	GENERATED_BODY()
 
   UPROPERTY(EditAnywhere, Category = "RangedEquipmentProperties|AimOffsets")
-  TObjectPtr<UAimOffsetBlendSpace> StandardAimOffset;
+  TObjectPtr<UBlendSpace> StandardAimBlendSpace;
+
+  UFUNCTION(BlueprintCallable, BlueprintPure)
+  inline UBlendSpace* GetStandardAimBlendSpace() { return StandardAimBlendSpace; }
 };
