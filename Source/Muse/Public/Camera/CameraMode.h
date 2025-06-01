8b686a35b92a10f249eb7177c2eab354a8036ea9
TristanM <95954032+TMTwittles@users.noreply.github.com>
2025-06-01 20:30:18 +1000

    MM22: Default implementation of new camera component.

---
+++ Source/Muse/Public/Camera/CameraMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CameraMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUSE_API UCameraMode : public UObject
+{
+	GENERATED_BODY()
+
+
+};
